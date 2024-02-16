293f016343a5650b5382e86b48fe0b4888e5b1e9
Ana <48427687+lubari@users.noreply.github.com>
2024-02-16 19:11:40 -0300

    Agregar archivos de proyecto.

---
+++ ejemplo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            //int floar bool char
+            //doublem, decimal, long, short, string datetime
+            int a, b, c;
+#pragma warning disable CS0168 // La variable está declarada pero nunca se usa
+#pragma warning disable CS0168 // La variable está declarada pero nunca se usa
+            float d, f;
+#pragma warning restore CS0168 // La variable está declarada pero nunca se usa
+#pragma warning restore CS0168 // La variable está declarada pero nunca se usa
+            Console.WriteLine("Hola Como va?");
+            Console.WriteLine("Ingrese un numero");
+            a = int.Parse(Console.ReadLine());
+            b = 10; c = a + b;
+            
+            Console.WriteLine("El resultado es: " + c);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejemplo2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; int b = 12;
+
+            if(a == b)
+            {
+                //
+            }
+            else
+            {
+                //
+            }
+
+            switch (a)
+            {
+                case 0:
+                    break; 
+                case 1:
+                    break;
+                default: 
+                    break;
+            }
+
+            // ciclos
+            //for, while, do while
+            //++ -- += -=
+            //*= /=
+            for (int i = 1; i < a; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //vectores
+            //int, float, char, bool
+
+            int[] numeros = new int[10];
+            numeros[0] = 10;
+            numeros[5] = 7;
+
+            int j = numeros[5];
+
+            //funciones
+            Console.WriteLine(saludar("ana"));
+            sumar(2, ref b);
+
+            Console.ReadKey();
+        }
+
+        static int sumar(int a, ref int z)
+        {
+            return a + z;
+        }
+
+        static string saludar(string nombre)
+        {
+            return "hola " + nombre;
+        }
+    }
+}
